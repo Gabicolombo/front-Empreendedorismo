b43f3864b88c7fe9b6d266928e18b9ae2befc44c
Luan Monteiro <luan.monteiro@spacesheepgames.com>
2022-10-21 19:11:08 -0300

    Added logo to the header and added first page for travel plan creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 
 import { UserService } from './services/user';
 import { HomeComponent } from './home/home.component';
+import { ViagemCadastroPage1Component } from './viagem-cadastro-page1/viagem-cadastro-page1.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { HomeComponent } from './home/home.component';
     HeaderComponent,
     LoginComponent,
     CadastroComponent,
-    HomeComponent
+    HomeComponent,
+    ViagemCadastroPage1Component
   ],
   imports: [
     BrowserModule,
